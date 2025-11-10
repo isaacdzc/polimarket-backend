65f26c63d97bb4c67badda0108edee8197003d2d
Isaac Diaz <isaaccnkr@gmail.com>
2025-11-10 17:50:31 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ClientsModule } from './clients/clients.module';
+import { SalesmanModule } from './salesman/salesman.module';
+import { OrdersModule } from './orders/orders.module';
+
+@Module({
+  imports: [ProductsModule, ClientsModule, SalesmanModule, OrdersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
