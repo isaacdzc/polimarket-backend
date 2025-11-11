a265ba2ab49326f5b04ce002c063116cd82cdd44
Isaac Diaz <isaaccnkr@gmail.com>
2025-11-10 22:17:32 -0500

    update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { ClientsModule } from './clients/clients.module';
-import { SalesmanModule } from './salesman/salesman.module';
-import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, ClientsModule, SalesmanModule, OrdersModule],
+  imports: [ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
