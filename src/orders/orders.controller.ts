c715dbd21261337eb138d9342d94a11b04c49eb6
Isaac Diaz <isaaccnkr@gmail.com>
2025-11-13 20:21:21 -0500

    add orders/detail endpoint

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
@@ -8,5 +8,10 @@ export class OrdersController {
 	@Get('get-orders')
 	getOrders() {
 		return this.ordersService.getOrders();
+	}
+
+	@Get('detail/:id')
+	getOrderDetail(@Param('id') id: string) {
+		return this.ordersService.getOrderById(id);
 	}
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { orders } from './orders.data';
 import { Order } from './order.entity';
 
@@ -6,5 +6,13 @@ import { Order } from './order.entity';
 export class OrdersService {
 	async getOrders(): Promise<Order[]> {
 		return orders;
+	}
+
+	async getOrderById(id: string): Promise<Order> {
+		const order = orders.find((o) => o.id === id);
+		if (!order) {
+			throw new NotFoundException(`Order with id ${id} not found`);
+		}
+		return order;
 	}
 }
